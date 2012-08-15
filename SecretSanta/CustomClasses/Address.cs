2d782b893803b7556d33292866856d109607d54d
mgargol <Mathew.Gargol@gmail.com>
2012-08-15 12:44:52 -0700

    Should have been committed in last commit.

---
+++ SecretSanta/CustomClasses/Address.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace SecretSanta.CustomClasses
+{
+    [DataContract]
+    public class Address
+    {
+        [DataMember]
+        public string UnitOrSuite { get; set; }
+        [DataMember]
+        public string StreetNumber { get; set; }
+        [DataMember]
+        public string StreetName { get; set; }
+        [DataMember]
+        public string StreetType { get; set; }
+        [DataMember]
+        public string StreetDirection { get; set; }
+        [DataMember]
+        public string City { get; set; }
+        [DataMember]
+        public string Province { get; set; }
+        [DataMember]
+        public string PostalCode { get; set; }
+
+        public override string ToString()
+        {
+            var addressBuilder = new StringBuilder();
+            addressBuilder.Append(UnitOrSuite);
+
+            if (!string.IsNullOrEmpty(addressBuilder.ToString()))
+            {
+                addressBuilder.Append(" ");    
+            }
+
+            addressBuilder.Append(StreetNumber);
+
+            if (!string.IsNullOrEmpty(addressBuilder.ToString()))
+            {
+                addressBuilder.Append(" ");
+            }
+
+            addressBuilder.Append(StreetName);
+
+            if (!string.IsNullOrEmpty(addressBuilder.ToString()))
+            {
+                addressBuilder.Append(" ");
+            }
+
+            addressBuilder.Append(StreetType);
+
+            if (!string.IsNullOrEmpty(addressBuilder.ToString()))
+            {
+                addressBuilder.Append(" ");
+            }
+
+            addressBuilder.Append(StreetDirection);
+
+            if (!string.IsNullOrEmpty(addressBuilder.ToString()))
+            {
+                addressBuilder.Append(" ");
+            }
+
+            addressBuilder.Append(City);
+
+            if (!string.IsNullOrEmpty(addressBuilder.ToString()))
+            {
+                addressBuilder.Append(" ");
+            }
+
+            addressBuilder.Append(Province);
+
+            if (!string.IsNullOrEmpty(addressBuilder.ToString()))
+            {
+                addressBuilder.Append(" ");
+            }
+
+            addressBuilder.Append(PostalCode);
+
+            return addressBuilder.ToString();
+        }
+    }
+}
